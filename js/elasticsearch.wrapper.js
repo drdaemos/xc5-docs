a78ca3e2ff60c2a80747fd3d1263d967c1b49745
Eugene Dementjev <daemos@x-cart.com>
2016-07-27 12:04:49 +0400

    and again

--- js/elasticsearch.wrapper.js
+++ js/elasticsearch.wrapper.js
@@ -19,13 +19,12 @@ layout: null
   }
 
   Search.prototype.connection = {
-    host: [
-      {
-        host: '{{ site.elasticsearch.js_host }}',
-        auth: 'kbuser:53cr37p4',
-        port: (window.location.protocol === 'https:' ? ':8443' : ':8000')
-      }
-    ],
+    host: {
+      host: '{{ site.elasticsearch.js_host }}',
+      auth: 'kbuser:53cr37p4',
+      port: (window.location.protocol === 'https:' ? ':8443' : ':8000'),
+      protocol: (window.location.protocol === 'https:' ? 'https' : 'http')
+    },
     log:  'trace',
     apiVersion: '2.0'
   }
