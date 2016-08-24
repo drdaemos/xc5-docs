5acc1855f011644f35fee7cb0e97c8dc73851778
Eugene Dementjev <drdaemos@gmail.com>
2016-08-24 07:22:35 +0400

    Properly drop ES index

--- _plugins/elasticsearch.rb
+++ _plugins/elasticsearch.rb
@@ -190,10 +190,11 @@ module Jekyll
 
       # if db is not exists, create one.
       begin
+        es.delete_index
+        sleep 5
+        
         unless es.type_exists
-          es.delete_index
-          sleep 5
-          
+
           es.create_index
           # wait index created
           sleep 2
